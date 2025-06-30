d583b2ab1c5a4ea9370e9a90d723acaa5d6ba347
Harini Arunachalam <hariniaruna25@gmail.com>
2025-06-30 22:00:03 +0530

    LED_Pushbutton project added

---
+++ LED_Pushbutton/LED_Pushbutton/LED_Pushbutton.ino
@@ -0,0 +1,20 @@
+const int ledPin = 3;
+const int buttonPin = 2;
+
+int buttonState = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH); // Turn LED ON
+  } else {
+    digitalWrite(ledPin, LOW);  // Turn LED OFF
+  }
+}
+
